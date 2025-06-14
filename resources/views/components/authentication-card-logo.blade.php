09025553114db1a339bd41a6f10b37d97a5fa39d
Yahya OUAMAR <ouamar.yahya@gmail.com>
2025-06-14 19:59:08 +0200

    added chat view

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <img src="{{ asset('images/logo.png') }}" alt="ZillyChat Logo" class="block h-16 w-auto" />
+    <img src="{{ asset('images/logo.png') }}" alt="ZillyChat Logo" class="mx-auto lg:mx-0 block w-auto mb-4" style="height: 100px;" />
 </a>
